27fd3d33a1804d012d7441d2094cce76451cf506
cindy-cho <37435790+cindy-cho@users.noreply.github.com>
2021-11-07 15:44:28 +0900

    Delete portal_space directory

--- portal_space/src/utils/firebase/initFirebase.js
+++
@@ -1,9 +0,0 @@
-import firebase from 'firebase/app';
-import 'firebase/firestore';
-import firebaseConfig from './firebaseConfig';
-
-const app = firebase.initializeApp(firebaseConfig);
-const firestore = firebase.firestore();
-
-export const app;
-export const firestore;
